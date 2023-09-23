9bd264195d6d2ae3e939fdaa8fa4bf3a8b94caa9
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-09-23 16:37:53 +0100

    Update deps.mk

--- applications/pusher/deps.mk
+++ applications/pusher/deps.mk
@@ -1,6 +1,6 @@
 DEPS = apns fcm lager
 
-dep_apns = git https://github.com/2600hz/erlang-apns4erl.git aba1fa96a4abbbb2c1628ad5d604f482aad4d12f # latest commit SHA to 2600hz branch
+dep_apns = git https://github.com/2600hz/erlang-apns4erl.git 5b1d1c9aad31c367b9a144f3266d95fbc1147277 # latest commit SHA to 2600hz branch
 
 #dep_fcm = git https://github.com/2600hz/erlang-fcm.git b2f68a4c6f0f59475597a35e2dc9be13d9ba2910
 dep_fcm = git https://github.com/softwarejoint/fcm-erlang.git 1.2.1
